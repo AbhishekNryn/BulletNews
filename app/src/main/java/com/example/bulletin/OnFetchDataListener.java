a348f11124ca8834592aa96b7a4ebcefed58cc80
AbhishekNryn <narayanabhishek090@gmail.com>
2022-12-25 00:46:12 +0530

    Final commit

---
+++ app/src/main/java/com/example/bulletin/OnFetchDataListener.java
@@ -0,0 +1,16 @@
+package com.example.bulletin;
+
+import com.example.bulletin.Models.NewsHeadlines;
+
+import java.util.List;
+
+public class OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message) {
+
+    }
+
+    void onError(String message) {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/bulletin/SelectListener.java
@@ -0,0 +1,8 @@
+package com.example.bulletin;
+
+import com.example.bulletin.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+
+}
